9ed832edc7d18a758f6826f8aa2fd8fed7b25d73
Evan Martin <evan.martin@gmail.com>
2018-11-15 13:37:49 -0800

    use the first token in the file as the module link
    When a file imports another file, with syntax like
      import * as x from 'some/path';
    we want 'some/path' to refer to a VName that just means "the entire
    file".  It doesn't refer to any text in particular, so we just mark
    the first letter in the file as the anchor for this.
    
    Previously we used the first 'export' statement in the file, but this
    meant that the xref would jump you to a random spot within the file.

--- kythe/typescript/testdata/module.ts
+++ kythe/typescript/testdata/module.ts
@@ -3,9 +3,13 @@
 // without an extension).
 //- Mod=vname("module", _, _, "testdata/module", _).node/kind record
 
-// The first 'export' statement in the module is tagged as defining
-// the module.
-//- @"export" defines/binding Mod
-export var value = 3;
+// The first letter in the module is tagged as defining the module.
+// See discussion on emitModuleAnchor().
+//- ModAnchor.node/kind anchor
+//- ModAnchor./kythe/loc/start 0
+//- ModAnchor./kythe/loc/end 1
+//- ModAnchor defines/binding Mod
+
+export let value = 3;
 
 export type MyType = string;
